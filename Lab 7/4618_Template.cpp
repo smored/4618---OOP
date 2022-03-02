8386d24df95109f2ddd707cf96881a3936f5d593
--help <kurtqa22@gmail.com>
2022-03-01 20:24:21 -0800

    started lab 7... first commit from pi

---
+++ Lab 7/4618_Template.cpp
@@ -0,0 +1,88 @@
+#include "CControl.h"
+
+using namespace std;
+enum { DIGITAL, ANALOG, SERVO };
+
+void lab3_demo() {
+    std::cout << "\n***********************************";
+    std::cout << "\n* ELEX4618 Lab 3 Demo";
+    std::cout << "\n***********************************";
+    std::cout << "\n(1) joystick_demo";
+    std::cout << "\n(2) DIO_demo";
+    std::cout << "\n(3) button_demo";
+    std::cout << "\n(4) servo_demo";
+    std::cout << "\nCMD> ";
+}
+
+// For lab 3 demo
+CControl comms;
+void joystick_demo();
+void DIO_demo();
+void button_demo();
+void servo_demo();
+#define MAXANALOG 1023.0f;
+
+
+int main(int argc, char* argv[])
+{
+    comms.init_com();
+    int cmd = -1;
+    do
+    {
+        lab3_demo();
+        std::cin >> cmd;
+        switch (cmd)
+        {
+        case 1: joystick_demo(); break;
+        case 2: DIO_demo(); break;
+        case 3: button_demo(); break;
+        case 4: servo_demo(); break;
+        }
+    } while (cmd != 0);
+}
+
+void joystick_demo() {
+    int xval, yval;
+    float xscale, yscale;
+    do {
+        yval = comms.get_analog(26);
+        yscale = (float)yval * 100 / MAXANALOG;
+        xval = comms.get_analog(2);
+        xscale = (float)xval * 100 / MAXANALOG;
+        //std::cout << setprecision(2);
+        std::cout << "Y AXIS: " << yval << "(" << yscale << "%)" << "\t";
+        std::cout << "X AXIS: " << xval << "(" << xscale << "%)" << endl;
+    } while (!GetAsyncKeyState(VK_ESCAPE));
+}
+
+void DIO_demo() {
+    // Digital IO test
+    int input;
+    do {
+        cout << "DIGITAL TEST: CH? = ";
+        cin >> input;
+        if (input < 0) return;
+        cout << endl;
+        comms.set_data(DIGITAL, 37, input);
+    } while (!GetAsyncKeyState(VK_ESCAPE));
+}
+
+void button_demo() {
+    int counter = 0;
+    // Debounced pushbutton test
+    do {
+        if (comms.get_button(33)) counter++;
+        cout << counter << endl;
+    } while (!GetAsyncKeyState(VK_ESCAPE));
+}
+
+void servo_demo() {
+    // Servo cycle test
+    do {
+        comms.set_servo(179);
+        Sleep(1000);
+        comms.set_servo(1);
+        Sleep(1000);
+    } while (!GetAsyncKeyState(VK_ESCAPE));
+
+}
---
+++ Lab 7/CControl.cpp
@@ -0,0 +1,226 @@
+#include "CControl.h"
+#include "Serial.h"
+#include <string>
+
+
+#define MAX_VAL 1024.0f
+#define DEBOUNCE_TIME 0
+#define TIMEOUT 100
+
+
+using namespace std;
+
+CControl::CControl() {
+
+}
+
+CControl::~CControl() {
+
+}
+
+bool CControl::getPort(int portNum) {
+	cout << "Opening COM: " << portNum;
+
+	const string strport = "COM";
+	Serial tempCom;
+	tempCom.open(strport + to_string(portNum));
+
+	if (tempCom.is_open()) {
+		cout << "\nwaiting for ACK...\n";
+		if (set_data(0, 37, 1, tempCom)) {
+			cout << "COM Port is open: " << portNum << endl;
+			//tempCom.~Serial();
+			return true;
+		}
+		else {
+			cout << "Error: No ACK on " << portNum << endl;
+		}
+	}
+	else {
+		cout << "\nError: could not open port\n";
+	}
+	//tempCom.~Serial();
+	return false;
+}
+
+void CControl::init_com(int port) {
+	const string strport = "COM";
+	_com.open(strport + to_string(port));
+	std::cout << "Opening Port " << strport + to_string(port) << endl;
+
+}
+
+void CControl::init_com() {
+	// Loop through all comm posibilities, check if open and then list available ports to choose from
+	const int MAX_PORTS = 255;
+	int comport;
+	while (1) {
+		for (comport = 0; comport < MAX_PORTS; comport++) {
+			if (getPort(comport)) break;
+		}
+
+		if (comport >= MAX_PORTS) {
+			cout << "Error: Could not find COM Port" << endl;
+		}
+		else {
+			_com.open("COM" + to_string(comport));
+			break;
+		}
+	}
+}
+
+bool CControl::get_data(int type, int channel, int& result) {
+	_com.flush();
+	string tx_str = "G ";
+	tx_str += to_string(type) + " " + to_string(channel) + '\n';
+
+	//cout << "get_data writing: " << tx_str << endl;
+	_com.write(tx_str.c_str(), tx_str.length());
+
+	string rx_str = "";
+	char buff[2]; buff[0] = 0;
+
+	auto start = std::chrono::system_clock::now();
+	auto now = std::chrono::system_clock::now();
+
+	while (buff[0] != '\n') { // go until \n character
+		if (std::chrono::duration_cast<std::chrono::milliseconds>(now - start).count() >= TIMEOUT) {
+			cout << "Error: get_data() timeout..." << endl;
+			return false;
+		}
+		if (_com.read(buff, 1) > 0) { // if stuff available
+			rx_str += buff[0]; // append buffer to string
+		}
+		now = std::chrono::system_clock::now();
+	}
+	//cout << "get_data value returned: " << rx_str << endl;
+	// Find and return the value of interest
+	int delimitcount = 0;
+	for (int i = 0; i < rx_str.length(); i++) {
+		if (rx_str[i] == ' ') delimitcount++;
+		if (delimitcount >= 3) {
+			rx_str.erase(0, i); // erase everything except last number
+			break;
+		}
+	}
+
+	// Remove '\n' and ' ' characters so stoi() doesnt blow up
+	if (rx_str[rx_str.length() - 1] == '\n') rx_str.erase(rx_str.length() - 1);
+	if (rx_str[0] == ' ') rx_str.erase(0,0);
+
+	result = stoi(rx_str);
+
+	return true;
+}
+
+bool CControl::set_data(int type, int channel, int val) {
+	_com.flush();
+	string tx_str = "S ", rx_str;
+	tx_str += to_string(type) + " " + to_string(channel) + " " + to_string(val) + '\n';
+	//cout << "set_data writing: " << tx_str << endl;
+	_com.write(tx_str.c_str(), tx_str.length());
+
+	// Check if data is acknowledged
+	auto start = std::chrono::system_clock::now();
+	auto now = std::chrono::system_clock::now();
+
+	char buff[2]; buff[0] = 0;
+
+	while (buff[0] != '\n') { // go until \n character
+		if (std::chrono::duration_cast<std::chrono::milliseconds>(now - start).count() >= TIMEOUT) {
+			cout << "Error: get_data() timeout..." << endl;
+			return false;
+		}
+		if (_com.read(buff, 1) > 0) { // if stuff available
+			rx_str += buff[0]; // append buffer to string
+		}
+		now = std::chrono::system_clock::now();
+	}
+
+	if (rx_str.empty()) {
+		return false;
+	}
+	else {
+		return true;
+	}
+}
+
+bool CControl::set_data(int type, int channel, int val, Serial &port) {
+	string tx_str = "S ";
+	tx_str += to_string(type) + " " + to_string(channel) + " " + to_string(val) + '\n';
+	port.write(tx_str.c_str(), tx_str.length());
+
+	// Check if data is acknowledged
+	std::chrono::time_point<std::chrono::system_clock> start = std::chrono::system_clock::now();
+	std::chrono::time_point<std::chrono::system_clock> now = std::chrono::system_clock::now();
+
+	char buff[2]; buff[0] = 0;
+	while (std::chrono::duration_cast<std::chrono::milliseconds>(now - start).count() <= 50) {
+		if (port.read(buff, 1) > 0) {
+			return true;
+		}
+		now = std::chrono::system_clock::now();
+	}
+	return false;
+}
+
+bool CControl::get_button(int channel, int BID) {
+
+	static bool reset[] = {0,0};
+	int result;
+	bool isPressed;
+
+	get_data(0, channel, result); isPressed = result == 0;
+
+	if (!reset[BID]) {
+		if (isPressed) {
+			reset[BID] = true;
+			return true;
+		}
+	}
+	else {
+		if (!isPressed) {
+			reset[BID] = false;
+		}
+	}
+
+	return false;
+
+	//int nPressed;
+	//static bool wasPressed = false;
+	///*float time1 = cv::getTickCount();
+	//static float time2 = cv::getTickCount();*/
+
+	//get_data(0, channel, nPressed);
+	//bool bPressed = nPressed == 0;
+
+	//	// changed
+	//	if (bPressed && !wasPressed) {
+	//		wasPressed = false;
+	//		return true;
+	//	}
+
+	//if (bPressed) {
+	//	wasPressed = true;
+	//	//time2 = cv::getTickCount();
+	//}
+
+	//return false;
+
+}
+
+// Return as a percentage of full range
+float CControl::get_analog(int channel) {
+	int result;
+	get_data(1, channel, result);
+	return (float)result/1023.0f;
+}
+
+void CControl::set_servo(int setPos, int channel) {
+	int getPos;
+	//do {
+		set_data(2, channel, setPos);
+	//	get_data(3, channel, getPos);
+	//} while (getPos != setPos);
+		// ^ this freezes up code - very not good
+}
