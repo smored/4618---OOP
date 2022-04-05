f97e1be98a771cafaed8c6df6bb07302c5cd799c
smored <62521837+smored@users.noreply.github.com>
2022-04-05 10:25:11 -0700

    client done

--- Lab 9/Client/4618_Client.cpp
+++ Lab 9/Client/4618_Client.cpp
@@ -11,40 +11,90 @@
 
 #include "Client.h"
 
-Client client(4618, "127.0.0.1");
+Client client(4618, "10.0.0.134");
+std::mutex txLock;
 
 void print_menu()
 {
 	std::cout << "\n***********************************";
 	std::cout << "\n* ELEX4618 Client Project";
 	std::cout << "\n***********************************";
-	std::cout << "\n(1) Send command";
-	std::cout << "\n(2) Get image";
-	std::cout << "\n(3) Send A";
+	//std::cout << "\n(1) Get image";
+	std::cout << "\n(1) Send A";
+	std::cout << "\n(2) Toggle System MANUAL/AUTOMATIC";
+	std::cout << "\n(3) Sort Ball Left";
+	std::cout << "\n(4) Sort Ball Right";
 	std::cout << "\n(0) Exit";
 	std::cout << "\nCMD> ";
 }
 
-void send_command()
+void count_ball(std::string input) {
+	int delimit1 = -1, delimit2 = -1, delimit3 = -1;
+	int i = 0;
+	try {
+		for (char ch : input) { // Find location of delimiters
+			if (ch == ';') {
+				delimit1 = i;
+			}
+			else if (ch == ',') {
+				delimit2 = i;
+			}
+			else if (ch == '\n') {
+				delimit3 = i;
+			}
+			i++;
+		}
+	}
+	catch (std::exception e) {
+		std::cerr << e.what() << " in for each" << std::endl;
+	}
+
+	if (delimit1 == -1 || delimit2 == -1 || delimit3 == -1) {
+		std::cerr << "Error, Invalid String\n";
+		return;
+	}
+
+	// Read numbers based on delimiters
+	try {
+		std::string num1 = input.substr(delimit1+1, delimit2-1 - delimit1);
+		std::cout << "Ball Left Count: " << num1 << std::endl;
+		std::string num2 = input.substr(delimit2+1, delimit3 - delimit2);
+		std::cout << "Ball Right Count: " << num2 << std::endl;
+	}
+	catch (std::exception e) {
+		std::cerr << e.what() << " in substring" << std::endl;
+	}
+}
+
+void send_command(std::string cmd)
 {
 	std::string str;
-	client.tx_str("cmd");
+
+	txLock.lock();
+	client.tx_str(cmd);
+	txLock.unlock();
 
 	do
 	{
+		txLock.lock();
 		client.rx_str(str);
+		txLock.unlock();
 		if (str.length() > 0)
 		{
 			std::cout << "\nClient Rx: " << str;
+			count_ball(str);
 		}
 	} while (str.length() == 0);
 }
 
 
-void send_a()
+void send_a()// deprecated
 {
 	std::string str;
+
+	txLock.lock();
 	client.tx_str("a");
+	txLock.unlock();
 
 	do
 	{
@@ -59,25 +109,50 @@ void send_a()
 void get_image()
 {
 	cv::Mat im;
-	int count = 0;
+	bool rx_success = true;
 
-  client.tx_str("im");
+  do {
 
-	if (client.rx_im(im) == true)
-	{
-		if (im.empty() == false)
-		{
-			count++;
-			std::cout << "\nImage received: " << count;
-			cv::imshow("rx", im);
-			cv::waitKey(100);
-		}
-	}
+	  if (rx_success) {
+		  txLock.lock();
+		  client.tx_str("im");
+		  txLock.unlock();
+	  }
+
+	  txLock.lock();
+	  if (client.rx_im(im)) {
+		  rx_success = true;
+	  }
+	  else {
+		  rx_success = false;
+	  }
+	  txLock.unlock();
+
+	  if (rx_success && !im.empty()) {
+		  cv::imshow("rx", im);
+	  }
+
+	  /*
+	  if (client.rx_im(im)) { 
+		  rx_success = true;
+		  if (im.empty() == false)
+		  {
+			  cv::imshow("rx", im);
+		  }
+	  }
+	  else {
+		  rx_success = false;
+	  }*/
+
+  } while (cv::waitKey(100) != 'q');
 }
 
 
 int main(int argc, char* argv[])
 {
+	std::thread t1(&get_image);
+	t1.detach();
+
 	int cmd = -1;
 	do
 	{
@@ -85,9 +160,15 @@ int main(int argc, char* argv[])
 		std::cin >> cmd;
 		switch (cmd)
 		{
-		case 1: send_command(); break;
-		case 2: get_image(); break;
-		case 3: send_a(); break;
+		//case 1: send_command(); break;
+		//case 1: get_image(); break;
+		case 1: send_a(); break;
+		case 2: send_command("toggle"); break;
+		case 3: send_command("left"); break;
+		case 4: send_command("right"); break;
+		default: continue;
 		}
 	} while (cmd != 0);
+
+	//count_ball("18;25,34\n");
 }
